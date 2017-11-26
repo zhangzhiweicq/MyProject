86bfddf3adef6a41bd9f17217ce370c63ab1ecb2
Darren Zhang <zhangzhiwei_mail@sina.com>
2017-11-26 22:34:53 +0800

    我的提交！

---
+++ Silverlight2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Windows.Interop;
+using System.Windows.Browser;
+
+namespace Silverlight2
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+    
+}
